3169743939766de9e26b22a905f22ef0772855a4
dhiggins03 <dan.higgins2003@gmail.com>
2022-08-16 12:00:28 +0100

    addgitignore
    added first migration

---
+++ code/application/migrations/2022081611400_first_migration.php
@@ -0,0 +1,35 @@
+<?php
+
+class Migration_ extends CI_Migration {
+	// load dependencies
+	public function __construct()
+	{
+		$this->load->database();
+		$this->load->dbforge();
+	}
+
+  public function up()
+	{
+    $this->db->trans_begin();
+
+    $this->db->query("CREATE TABLE `users` (
+        `userId` int(11) NOT NULL AUTO_INCREMENT,
+        `userForename` varchar(255) NOT NULL,
+        `userSurname` varchar(255) NOT NULL,
+        `userEmail` varchar(255) NOT NULL,
+        PRIMARY KEY (`userId`)
+      ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;");
+
+
+
+
+      if ($this->db->trans_status() === FALSE)
+      {
+          $this->db->trans_rollback();
+      } 
+      else
+      {
+          $this->db->trans_commit();
+      }
+  }
+  }
